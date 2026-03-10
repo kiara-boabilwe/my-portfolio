38b0c4c9140481a30dcd0f5623b461caa97cd60b
Kiara Boabilwe <pinkkiara55@gmail.com>
2026-03-10 16:10:15 +0200

    Update

---
+++ script.js
@@ -0,0 +1,19 @@
+const toggleButton = document.getElementById('theme-toggle'); 
+
+ 
+
+toggleButton.addEventListener('click', function() { 
+
+    document.body.classList.toggle('dark-mode'); 
+
+    if (document.body.classList.contains('dark-mode')) { 
+
+        toggleButton.textContent = 'Light Mode'; 
+
+    } else { 
+
+        toggleButton.textContent = 'Dark Mode'; 
+
+    } 
+
+}); 
